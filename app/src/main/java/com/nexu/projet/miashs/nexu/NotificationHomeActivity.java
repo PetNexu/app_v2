6ef97de0af3ce4344f1931315a069635da7defdc
charleneTU <charleneturbe31@gmail.com>
2018-05-02 17:50:27 +0200

    cha

--- app/src/main/java/com/nexu/projet/miashs/nexu/NotificationHomeActivity.java
+++ app/src/main/java/com/nexu/projet/miashs/nexu/NotificationHomeActivity.java
@@ -1,23 +1,23 @@
 package com.nexu.projet.miashs.nexu;
 
-public class NotificationHomeActivity {
-   /* package com.tutos.android.notification;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 
-    public class TutoNotificationHomeActivity extends Activity {
+public class NotificationHomeActivity extends AppCompatActivity{
+
         private Button addNotificationBtn;
         private Button deleteNotificationBtn;
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.main);
+            setContentView(R.layout.notification);
 
             addNotificationBtn = (Button) findViewById(R.id.add_notification);
             addNotificationBtn.setOnClickListener(new OnClickListener() {
@@ -37,5 +37,23 @@ import android.widget.Toast;
                 }
             });
         }
+
+      /*private final void createNotification(){
+        final NotificationManager mNotification = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        final Intent launchNotifiactionIntent = new Intent(this, TutoNotificationHomeActivity.class);
+        final PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                REQUEST_CODE, launchNotifiactionIntent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Notification.Builder builder = new Notification.Builder(this)
+                .setWhen(System.currentTimeMillis())
+                .setTicker(notificationTitle)
+                .setSmallIcon(R.drawable.notification)
+                .setContentTitle(getResources().getString(R.string.notification_title))
+                .setContentText(getResources().getString(R.string.notification_desc))
+                .setContentIntent(pendingIntent);
+
+        mNotification.notify(NOTIFICATION_ID, builder.build());
     }*/
 }
